67af96fb53050cb85510f0dde1f1de5a1db03efb
GSB0203 <gimseungbin0203@gmail.com>
2025-07-16 00:26:02 +0900

    remove:: 불필요해진 파일 제거

--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../auth/context';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
