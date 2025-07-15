80dd1ba91532f3662cb12ee074141df7dbb0d129
GSB0203 <gimseungbin0203@gmail.com>
2025-07-16 04:00:17 +0900

    fix :: build 문제 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./auth/context";
 import { ChildProvider } from "./contexts/ChildContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <ChildProvider>
-          {children}
-        </ChildProvider>
+        <AuthProvider>
+          <ChildProvider>
+            {children}
+          </ChildProvider>
+        </AuthProvider>
       </body>
     </html>
   );
