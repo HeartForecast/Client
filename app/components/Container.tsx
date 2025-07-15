b91249bcd6bd234f12c857e55b212566617f93a3
GSB0203 <gimseungbin0203@gmail.com>
2025-07-16 04:38:04 +0900

    fix :: 화면 비율 문제 해결

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export default function Container({ children, className = "" }: ContainerProps) {
   return (
-    <div className={`flex flex-col items-center justify-center px-4 pt-10 pb-0 text-black ${className}`}>
+    <div className={`min-h-screen flex flex-col items-center justify-center px-4 pt-10 pb-0 text-black ${className}`}>
       {children}
     </div>
   );
